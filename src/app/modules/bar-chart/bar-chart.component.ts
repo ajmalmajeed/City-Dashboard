f4f1df2c1a298b3bba9c838cbc57555f92f3cc3a
Keshini Jayasinghe <keshinijayasinghe@Keshinis-MacBook-Pro.local>
2020-04-15 11:50:27 +0530

    chartjs color theming

--- src/app/modules/bar-chart/bar-chart.component.ts
+++ src/app/modules/bar-chart/bar-chart.component.ts
@@ -1,28 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-bar-chart',
-  templateUrl: './bar-chart.component.html',
-  styleUrls: ['./bar-chart.component.scss']
+  selector: "app-bar-chart",
+  templateUrl: "./bar-chart.component.html",
+  styleUrls: ["./bar-chart.component.scss"],
 })
 export class BarChartComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   public barChartOptions = {
     scaleShowVerticalLines: false,
-    responsive: true
+    responsive: true,
   };
-  public barChartLabels = ['January', 'February', 'March', 'April', 'May', 'June'];
-  public barChartType = 'bar';
+  public barChartLabels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+  ];
+  public barChartType = "bar";
   public barChartLegend = true;
   public barChartData = [
-    {data: [10, 17, 39, 50, 40, 75, 80], label: 'Pet Bottles'},
-    {data: [28, 30, 36, 40, 35, 55, 70], label: 'Tin Cans'},
-    {data: [15, 23, 35, 40, 38, 55, 67], label: 'Cardboard Boxes'}
+    {
+      data: [10, 17, 39, 50, 40, 75, 80],
+      label: "Pet Bottles",
+      backgroundColor: "#73bac7",
+      hoverBackgroundColor: "#73bac7",
+    },
+    {
+      data: [28, 30, 36, 40, 35, 55, 70],
+      label: "Tin Cans",
+      backgroundColor: "#94d4b4",
+      hoverBackgroundColor: "#94d4b4",
+    },
+    { data: [15, 23, 35, 40, 38, 55, 67], label: "Cardboard Boxes" },
   ];
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/modules/pie-chart/pie-chart.component.ts
+++ src/app/modules/pie-chart/pie-chart.component.ts
@@ -1,18 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pie-chart',
-  templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.scss']
+  selector: "app-pie-chart",
+  templateUrl: "./pie-chart.component.html",
+  styleUrls: ["./pie-chart.component.scss"],
 })
 export class PieChartComponent implements OnInit {
-  public pieChartLabels = ['Pet Bottles', 'Tin Cans', 'Cardboard Boxes'];
+  public pieChartLabels = ["Pet Bottles", "Tin Cans", "Cardboard Boxes"];
   public pieChartData = [26, 19, 15];
-  public pieChartType = 'pie';
+  public pieChartType = "pie";
 
-  constructor() { }
+  public pieChartColors = [
+    {
+      backgroundColor: ["#73bac7", "#94d4b4", "#f5d57d"],
+    },
+  ];
 
-  ngOnInit() {
-  }
+  constructor() {}
 
+  ngOnInit() {}
 }
